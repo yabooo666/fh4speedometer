9640d4ea475798dc7e934876cfae110ebdceeb19
Hopeless YABO <dangerous.yabana@icloud.com>
2026-06-05 04:12:14 +0200

    update fuel indicator, mileage & adjust speedometer on screen

--- html/js/hud.js
+++ html/js/hud.js
@@ -21,39 +21,39 @@ var s_enginehp;
 var s_lights;
 var s_mileage;
 
-$(function() {
-    window.addEventListener("message", function(event) {
+$(function () {
+    window.addEventListener("message", function (event) {
         var item = event.data;
-        
+
         if (item.ShowHud) {
-            inVehicle      = true;
-            s_PlayerID     = item.PlayerID;
-            s_Rpm          = item.CurrentCarRPM;
-            s_Speed        = item.CurrentCarSpeed;
-            s_Kmh          = item.CurrentCarKmh;
-            s_Mph          = item.CurrentCarMph;
-            s_Gear         = item.CurrentCarGear;
-            s_IL           = item.CurrentCarIL;
+            inVehicle = true;
+            s_PlayerID = item.PlayerID;
+            s_Rpm = item.CurrentCarRPM;
+            s_Speed = item.CurrentCarSpeed;
+            s_Kmh = item.CurrentCarKmh;
+            s_Mph = item.CurrentCarMph;
+            s_Gear = item.CurrentCarGear;
+            s_IL = item.CurrentCarIL;
             s_Acceleration = item.CurrentCarAcceleration;
-            s_Handbrake    = item.CurrentCarHandbrake;
-            s_ABS          = item.CurrentCarABS;
-            s_LS_r         = item.CurrentCarLS_r;
-            s_LS_o         = item.CurrentCarLS_o;
-            s_LS_h         = item.CurrentCarLS_h;
-            CalcSpeed      = s_Kmh;
-            s_fuel         = item.currentcarFuel;
-            s_indicators   = item.currentcarIndicators;
-            s_enginehp     = item.currentcarEngineHp;
-            s_lights       = item.currentcarLights;
-            s_mileage      = item.CurrentCarMileage || item.currentcarMileage || 0.0;
-            CalcRpm        = (s_Rpm * 9);
-            
-            if(CalcSpeed > 999) {
+            s_Handbrake = item.CurrentCarHandbrake;
+            s_ABS = item.CurrentCarABS;
+            s_LS_r = item.CurrentCarLS_r;
+            s_LS_o = item.CurrentCarLS_o;
+            s_LS_h = item.CurrentCarLS_h;
+            CalcSpeed = s_Kmh;
+            s_fuel = item.currentcarFuel;
+            s_indicators = item.currentcarIndicators;
+            s_enginehp = item.currentcarEngineHp;
+            s_lights = item.currentcarLights;
+            s_mileage = item.CurrentCarMileage || item.currentcarMileage || 0.0;
+            CalcRpm = (s_Rpm * 9);
+
+            if (CalcSpeed > 999) {
                 CalcSpeed = 999;
             }
-            
+
             // Vehicle Gear display
-            if(s_Gear == 0 && CalcRpm > 2) {
+            if (s_Gear == 0 && CalcRpm > 2) {
                 $(".geardisplay span").html("R");
                 $(".geardisplay").attr("style", "color: #FFF;border-color:#FFF;");
             } else if (s_Gear == 0) {
@@ -61,14 +61,14 @@ $(function() {
                 $(".geardisplay").attr("style", "color: #FFF;border-color:#FFF;");
             } else {
                 $(".geardisplay span").html(s_Gear);
-                if(CalcRpm > 7.5) {
+                if (CalcRpm > 7.5) {
                     $(".geardisplay").attr("style", "color: rgb(235,30,76);border-color:rgb(235,30,76);");
                 } else {
                     $(".geardisplay").removeAttr("style");
                 }
-                if(CalcRpm >= 9) {
+                if (CalcRpm >= 9) {
                     IsOverLoad = true;
-                    if(OverLoadRPM) {
+                    if (OverLoadRPM) {
                         CalcRpm = 9;
                         s_Rpm = 9;
                         OverLoadRPM = false;
@@ -82,25 +82,25 @@ $(function() {
                     IsOverLoad = false;
                 }
             }
-            
+
             // Vehicle RPM display
             $("#rpmshow").attr("data-value", CalcRpm.toFixed(2));
-            
+
             // Vehicle speed display
-            if(CalcSpeed >= 100) {
+            if (CalcSpeed >= 100) {
                 var tmpSpeed = Math.floor(CalcSpeed) + '';
                 speedText = '<span class="int1">' + tmpSpeed.substr(0, 1) + '</span><span class="int2">' + tmpSpeed.substr(1, 1) + '</span><span class="int3">' + tmpSpeed.substr(2, 1) + '</span>';
-            } else if(CalcSpeed >= 10 && CalcSpeed < 100) {
+            } else if (CalcSpeed >= 10 && CalcSpeed < 100) {
                 var tmpSpeed = Math.floor(CalcSpeed) + '';
                 speedText = '<span class="gray int1">0</span><span class="int2">' + tmpSpeed.substr(0, 1) + '</span><span class="int3">' + tmpSpeed.substr(1, 1) + '</span>';
-            } else if(CalcSpeed > 0 && CalcSpeed < 10) {
+            } else if (CalcSpeed > 0 && CalcSpeed < 10) {
                 speedText = '<span class="gray int1">0</span><span class="gray int2">0</span><span class="int3">' + Math.floor(CalcSpeed) + '</span>';
             } else {
                 speedText = '<span class="gray int1">0</span><span class="gray int2">0</span><span class="gray int3">0</span>';
             }
-            
+
             // Handbrake
-            if(s_Handbrake) {
+            if (s_Handbrake) {
                 $('.handbrake-icon').css({
                     'opacity': '1.0',
                     'filter': 'grayscale(0) brightness(1.0) drop-shadow(3px 5px 2px rgba(241, 26, 26, 0.61))'
@@ -111,33 +111,41 @@ $(function() {
                     'filter': 'grayscale(1) brightness(0.5) drop-shadow(0px 0px 0px transparent)'
                 });
             }
-            
+
             // Fuel system
             var fuelVal = Math.floor(s_fuel);
             if (fuelVal < 0) fuelVal = 0;
             if (fuelVal > 100) fuelVal = 100;
-            $(".fueldisplay span").html(fuelVal + "%");
+            
+            var maxLiters = 65;
+            var currentLiters = Math.round((s_fuel / 100) * maxLiters);
+            if (currentLiters < 0) currentLiters = 0;
+            if (currentLiters > maxLiters) currentLiters = maxLiters;
+            $(".fuel-text span").html(currentLiters + " \\ " + maxLiters + " L");
+
+            var dashoffset = 270 - (fuelVal / 100) * 270;
+            $(".fuel-fill").css({ 'stroke-dashoffset': dashoffset });
 
             if (s_fuel > 80.0) {
-                $('.pump svg').css({'fill': `#209d05`});
-                $('.pump svg').css({'filter': `drop-shadow(3px 5px 2px #209d059c)`});
-                $('.fueldisplay').css({'color': `#209d05`});
+                $('.fuel-fill').css({ 'stroke': `#209d05` });
+                $('.fuel-icon').css({ 'fill': `#209d05`, 'filter': `drop-shadow(0px 0px 4px rgba(32, 157, 5, 0.6))` });
+                $('.fuel-text').css({ 'color': `#209d05` });
             } else if (s_fuel > 60.0) {
-                $('.pump svg').css({'fill': `#86e62c`});
-                $('.pump svg').css({'filter': `drop-shadow(3px 5px 2px #86e62c9c)`});
-                $('.fueldisplay').css({'color': `#86e62c`});
+                $('.fuel-fill').css({ 'stroke': `#86e62c` });
+                $('.fuel-icon').css({ 'fill': `#86e62c`, 'filter': `drop-shadow(0px 0px 4px rgba(134, 230, 44, 0.6))` });
+                $('.fuel-text').css({ 'color': `#86e62c` });
             } else if (s_fuel > 40.0) {
-                $('.pump svg').css({'fill': `#ebff0a`});
-                $('.pump svg').css({'filter': `drop-shadow(3px 5px 2px #ebff0a9c)`});
-                $('.fueldisplay').css({'color': `#ebff0a`});
+                $('.fuel-fill').css({ 'stroke': `#ebff0a` });
+                $('.fuel-icon').css({ 'fill': `#ebff0a`, 'filter': `drop-shadow(0px 0px 4px rgba(235, 255, 10, 0.6))` });
+                $('.fuel-text').css({ 'color': `#ebff0a` });
             } else if (s_fuel > 20.0) {
-                $('.pump svg').css({'fill': `#f3ce03`});
-                $('.pump svg').css({'filter': `drop-shadow(3px 5px 2px #f3ce039c)`});
-                $('.fueldisplay').css({'color': `#f3ce03`});
+                $('.fuel-fill').css({ 'stroke': `#f3ce03` });
+                $('.fuel-icon').css({ 'fill': `#f3ce03`, 'filter': `drop-shadow(0px 0px 4px rgba(243, 206, 3, 0.6))` });
+                $('.fuel-text').css({ 'color': `#f3ce03` });
             } else if (s_fuel > 0.0) {
-                $('.pump svg').css({'fill': `#fe0a0a`});
-                $('.pump svg').css({'filter': `drop-shadow(3px 5px 2px #fe0a0a9c)`});
-                $('.fueldisplay').css({'color': `#fe0a0a`});
+                $('.fuel-fill').css({ 'stroke': `#fe0a0a` });
+                $('.fuel-icon').css({ 'fill': `#fe0a0a`, 'filter': `drop-shadow(0px 0px 6px rgba(254, 10, 10, 0.9))` });
+                $('.fuel-text').css({ 'color': `#fe0a0a` });
             }
 
             // Indicators
@@ -167,32 +175,32 @@ $(function() {
 
             // Engine health
             if (s_enginehp > 800.0) {
-                $('.engineHp svg').css({'fill': `#209d05`});
-                $('.engineHp svg').css({'filter': `drop-shadow(3px 5px 2px #209d059c)`});
+                $('.engineHp svg').css({ 'fill': `#209d05` });
+                $('.engineHp svg').css({ 'filter': `drop-shadow(3px 5px 2px #209d059c)` });
             } else if (s_enginehp > 600.0) {
-                $('.engineHp svg').css({'fill': `#86e62c`});
-                $('.engineHp svg').css({'filter': `drop-shadow(3px 5px 2px #86e62c9c)`});
+                $('.engineHp svg').css({ 'fill': `#86e62c` });
+                $('.engineHp svg').css({ 'filter': `drop-shadow(3px 5px 2px #86e62c9c)` });
             } else if (s_enginehp > 400.0) {
-                $('.engineHp svg').css({'fill': `#ebff0a`});
-                $('.engineHp svg').css({'filter': `drop-shadow(3px 5px 2px #ebff0a9c)`});
+                $('.engineHp svg').css({ 'fill': `#ebff0a` });
+                $('.engineHp svg').css({ 'filter': `drop-shadow(3px 5px 2px #ebff0a9c)` });
             } else if (s_enginehp > 200.0) {
-                $('.engineHp svg').css({'fill': `#f3ce03`});
-                $('.engineHp svg').css({'filter': `drop-shadow(3px 5px 2px #f3ce039c)`});
+                $('.engineHp svg').css({ 'fill': `#f3ce03` });
+                $('.engineHp svg').css({ 'filter': `drop-shadow(3px 5px 2px #f3ce039c)` });
             } else if (s_enginehp > 0.0) {
-                $('.engineHp svg').css({'fill': `#fe0a0a`});
-                $('.engineHp svg').css({'filter': `drop-shadow(3px 5px 2px #fe0a0a9c)`});
+                $('.engineHp svg').css({ 'fill': `#fe0a0a` });
+                $('.engineHp svg').css({ 'filter': `drop-shadow(3px 5px 2px #fe0a0a9c)` });
             }
 
             // Headlights
             if (s_lights == 1) {
-                $('.headlights svg').css({'fill': `#209d05`});
-                $('.headlights svg').css({'filter': `drop-shadow(3px 5px 2px #209d059c)`});
+                $('.headlights svg').css({ 'fill': `#209d05` });
+                $('.headlights svg').css({ 'filter': `drop-shadow(3px 5px 2px #209d059c)` });
             } else if (s_lights == 2) {
-                $('.headlights svg').css({'fill': `#0fa4e9`});
-                $('.headlights svg').css({'filter': `drop-shadow(3px 5px 2px #0fa4e99c)`});
+                $('.headlights svg').css({ 'fill': `#0fa4e9` });
+                $('.headlights svg').css({ 'filter': `drop-shadow(3px 5px 2px #0fa4e99c)` });
             } else if (s_lights == 0) {
-                $('.headlights svg').css({'fill': `rgba(26, 26, 26, 0.521)`});
-                $('.headlights svg').css({'filter': `drop-shadow(3px 5px 2px rgba(26, 26, 26, 0.521))`});
+                $('.headlights svg').css({ 'fill': `rgba(26, 26, 26, 0.521)` });
+                $('.headlights svg').css({ 'filter': `drop-shadow(3px 5px 2px rgba(26, 26, 26, 0.521))` });
             }
 
 
@@ -208,7 +216,7 @@ $(function() {
             // Display speed and container
             $(".speeddisplay").html(speedText);
             $("#container").fadeIn(500);
-            
+
         } else if (item.HideHud) {
             // Hide GUI
             $("#container").fadeOut(500);
@@ -216,18 +224,18 @@ $(function() {
         }
     });
 
-    setInterval(function() {
-        if(((o_rpm != s_Rpm)) || IsOverLoad) {
-            if(o_rpm - s_Rpm > 0.1 || s_Rpm - o_rpm > 0.1 || IsOverLoad) {
-                if(!hasFilter) {
-                    if(s_Rpm > o_rpm) {
-                        if(s_Acceleration) {
+    setInterval(function () {
+        if (((o_rpm != s_Rpm)) || IsOverLoad) {
+            if (o_rpm - s_Rpm > 0.1 || s_Rpm - o_rpm > 0.1 || IsOverLoad) {
+                if (!hasFilter) {
+                    if (s_Rpm > o_rpm) {
+                        if (s_Acceleration) {
                             $("#rpmshow").css('filter', 'url(#blur3)');
                         } else {
                             $("#rpmshow").css('filter', 'url(#blur2)');
                         }
                     } else {
-                        if(s_Acceleration) {
+                        if (s_Acceleration) {
                             $("#rpmshow").css('filter', 'url(#blur3)');
                         } else {
                             $("#rpmshow").css('filter', 'url(#blur2)');
@@ -236,14 +244,14 @@ $(function() {
                     hasFilter = true;
                 }
             } else {
-                if(hasFilter) {
+                if (hasFilter) {
                     $("#rpmshow").css('filter', 'drop-shadow(0px 0px 4px rgba(0,0,0,0.1))');
                     hasFilter = false;
                 }
             }
             o_rpm = s_Rpm;
         } else {
-            if(hasFilter) {
+            if (hasFilter) {
                 $("#rpmshow").css('filter', 'drop-shadow(0px 0px 4px rgba(0,0,0,0.1))');
                 hasFilter = false;
             }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-this script is not standalone. it is made for my framework. so some features will not work without my framework.
+check https://github.com/deweaponx/dx-fh4speed for real standalone version.
 
-check https://github.com/deweaponx/dx-fh4speed for standalone version.
+i'm not very good at Lua so i'll be using just HTML from here and rewrite script for my TS frimework.
+
+html is done and its working fine but i won't guarantee anything about client and server scripts since i'm not gooing to use them for my server.
+
+i'm not gonna maintain this script.
